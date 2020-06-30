e43645b0cbb106d0c5b58e5b54753b729776d09e
allden <30358515+allden@users.noreply.github.com>
2020-06-30 03:19:55 +0300

    Updates

---
+++ precache-manifest.21c5606c738b97fc908ec283ec1624d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87dc06634aababef6a4d2469e7fb7f43",
+    "url": "/marvel-explore/index.html"
+  },
+  {
+    "revision": "75eccab0f439b04a6fed",
+    "url": "/marvel-explore/static/css/main.781a6648.chunk.css"
+  },
+  {
+    "revision": "16a90226e72f8f940359",
+    "url": "/marvel-explore/static/js/2.829468b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/marvel-explore/static/js/2.829468b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eccab0f439b04a6fed",
+    "url": "/marvel-explore/static/js/main.afa90621.chunk.js"
+  },
+  {
+    "revision": "6aa9f25c98cbb20ac97f",
+    "url": "/marvel-explore/static/js/runtime-main.29b3978b.js"
+  },
+  {
+    "revision": "c9f398bdd42b2492acf79fa41af7d1d3",
+    "url": "/marvel-explore/static/media/captain-marvel-banner.c9f398bd.jpg"
+  },
+  {
+    "revision": "bc3b11bdb20920dc62e47c9dbef0a302",
+    "url": "/marvel-explore/static/media/iron-man-banner.bc3b11bd.jpg"
+  },
+  {
+    "revision": "38aa7a3b45213fb445a853586f86524a",
+    "url": "/marvel-explore/static/media/she-hulk-banner.38aa7a3b.jpg"
+  },
+  {
+    "revision": "2ca5b1eceed70ae248a837ba34ddd497",
+    "url": "/marvel-explore/static/media/thor-banner.2ca5b1ec.jpg"
+  }
+]);
