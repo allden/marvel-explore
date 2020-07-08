b5a9400439dad98aea659161613e85b1fb1d4b80
Allden <lordofdusk@protonmail.com>
2020-07-08 19:03:17 +0300

    Changed footer copyright date.

--- src/components/general/Footer.js
+++ src/components/general/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer">
-            Designed by Allden @ Github, &copy; 2019 All Rights Reserved
+            Designed by Allden @ Github, &copy; 2020 All Rights Reserved
         </footer>
     );
 };
